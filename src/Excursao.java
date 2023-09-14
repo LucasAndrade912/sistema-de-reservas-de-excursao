036dc71f96b57a5870d472d77e862db523b99a50
Lucas Andrade dos Santos <20222370031@ad.ifpb.edu.br>
2023-09-14 13:48:00 -0300

    iniciando classe excursao

---
+++ src/Excursao.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Excursao {
+    private int codigo;
+    private double precoPorPessoa;
+    private int limiteDeReservas;
+    private ArrayList<String> reservas = new ArrayList<>();
+
+
+}
