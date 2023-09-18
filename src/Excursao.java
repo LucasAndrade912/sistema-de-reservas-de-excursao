997d5d2ff1937af28f5d51960e618fc62ffd23a3
Lucas Andrade <lucasantos.dev@gmail.com>
2023-09-18 12:24:33 -0300

    adicionando construtores na classe Excursao

--- src/Excursao.java
+++ src/Excursao.java
@@ -6,13 +6,21 @@ public class Excursao {
     private int limiteDeReservas;
     private ArrayList<String> reservas = new ArrayList<>();
 
-    public Excursao(int codigo, double precoPorPessoa, int limiteDeReservas) {
-        this.codigo = codigo;
-        this.precoPorPessoa = precoPorPessoa;
-        this.limiteDeReservas = limiteDeReservas;
+    public Excursao(int codigo, double precoPorPessoa, int limiteDeReservas) throws Exception {
+        if (codigo > 0 && precoPorPessoa > 0 && limiteDeReservas > 0) {
+            this.codigo = codigo;
+            this.precoPorPessoa = precoPorPessoa;
+            this.limiteDeReservas = limiteDeReservas;
+        }
+
+        throw new Exception("Os dados passados para excursão precisam ser maior que 0.");
     }
 
-    public Excursao(int codigo) {
-        this.codigo = codigo;
+    public Excursao(int codigo) throws Exception {
+        if (codigo > 0) {
+            this.codigo = codigo;
+        }
+
+        throw new Exception("O código da excursão precisa ser maior que 0.");
     }
 }
